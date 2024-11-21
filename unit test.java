57be6a4bd3aa6004a744b7b446d27ef2a38f9a95
ST10363604 <parrynetili@gmail.com>
2024-11-21 16:37:54 +0200

    Create unit test.java

---
+++ unit test.java
@@ -0,0 +1,33 @@
+package com.mycompany.st10363604;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+class ST10363604Test {
+
+    void setUp() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void testDeveloperArrayPopulation() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void testDeleteReportsTask() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void testSamanthaPaulsonTasks() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void testCreateLoginTask() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void testLongestDurationTask() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
